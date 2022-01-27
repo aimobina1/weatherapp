be3afe98c9b5118ab9a7f7a340ca92ded0ce09fb
aimobina1 <aiworkgoal@gmail.com>
2022-01-27 19:41:44 +0330

    refrence to the hml objects and  update the  ui and adds the detail to the html

---
+++ script/app.js
@@ -0,0 +1,18 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img')
+
+const updateUI = (data) => {  //this data will be same as the objects we return in city updates and at the end in form submit
+  // destructure properties
+  const { cityDets, weather } = data;
+  // update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
