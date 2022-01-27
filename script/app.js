8e2b99af3654ebc60e12455e163ace20aebc2edf
aimobina1 <aiworkgoal@gmail.com>
2022-01-27 19:47:17 +0330

    set form submit and reset the input value

--- script/app.js
+++ script/app.js
@@ -35,7 +35,14 @@ const updateCity = async (city) => {
   const cityDets = await getCity(city);
   const weather = await getWeather(cityDets.Key);
   return { cityDets, weather };
-   // cityDets:cityDets, 
-  // weather:weather
 
 };
+
+cityForm.addEventListener('submit', e => {
+    // prevent default action
+    e.preventDefault();
+    
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+  
